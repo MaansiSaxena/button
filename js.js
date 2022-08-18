01abc37aff55690b0603dbabf6d284d5b010a25b
MaansiSaxena <110008099+MaansiSaxena@users.noreply.github.com>
2022-08-18 17:08:12 +0530

    Add files via upload

---
+++ js.js
@@ -0,0 +1,23 @@
+<script type="text/javascript">
+
+document.getElementById("mybtn").onclick=function()
+{document.getElementById("text").innerHTML ="I am a trainee at bhavna corp!";
+    
+ document.getElementById("secondbtn").innerHTML ="I Think " + document.getElementById("secondpar").innerHTML + "Awesome";
+    
+}
+    
+document.getElementById("mybbtn").onclick=function(){
+    document.getElementById("yellowbtn").style.backgroundColor="red";
+    }
+    
+function myfunction(){
+    document.getElementById("para").style.color ="blue"
+}
+    
+function mefunction(){
+    document.getElementById("circle3").style.visibility="hidden";
+    alert("Green Button Will Dissapear after you click Ok!")
+                }
+    
+</script>
